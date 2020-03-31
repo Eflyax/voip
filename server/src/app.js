cadb01b292ccad00d13ebf6431a0a5613afd45d8
Jakub Záruba <info@jakub-zaruba.cz>
2020-03-31 22:16:44 +0200

    Server is based on typescript

--- server/src/app.js
+++ server/src/app.js
@@ -1,29 +1,25 @@
-var https = require('https');
-var fs = require("fs");
+import https from 'https';
+import { createConnection, getManager } from 'typeorm';
 var WebSocketServer = require('ws').Server;
+import fs from 'fs';
 
 var wsPort = 5000;
 var masterId;
 var listeners = {};
-
 var httpsServer = https.createServer({
   key: fs.readFileSync('/etc/letsencrypt/live/voip.eflyax.cz/privkey.pem', 'utf8'),
   cert: fs.readFileSync('/etc/letsencrypt/live/voip.eflyax.cz/fullchain.pem', 'utf8')
 }).listen(wsPort);
 
-var wss = new WebSocketServer({server: httpsServer});
+var wss = new WebSocketServer({ server: httpsServer });
 
 var connectionId = 0;
 wss.on('connection', function (ws) {
-
   console.log('Client connected');
   connectionId = 1;
   listeners[connectionId] = ws;
 
   ws.on('message', function (message) {
-
-    // for (var cid in listeners) {
-
     listeners[connectionId].send(message, {
       binary: true
     }, function (err) {
@@ -32,8 +28,6 @@ wss.on('connection', function (ws) {
       }
     });
 
-
-    // }
   });
   //     console.log('Speaker connected');
   // } else {
@@ -42,16 +36,9 @@ wss.on('connection', function (ws) {
   //     console.log('Listener connected');
   // }
 
-
   ws.on('close', function () {
-    // if (isMaster) {
-    //   masterId = null;
-    //   console.log('Speaker disconnected');
-    // } else {
     delete listeners[connectionId];
     console.log('Client disconnected');
     // }
   });
 });
-
-console.log('Listening on port:', wsPort);
---
+++ server/src/index.js
@@ -0,0 +1,4 @@
+// Imports the babel library to transpile our code
+require("babel-core/register")
+
+exports = module.exports = require("./app")
